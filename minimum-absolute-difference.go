942f0cf012d7475defaab371d3e3b93e2cdc1ba1
John McKee <jmckee3@mac.com>
2019-12-13 15:35:50 -0800

    straight forward version

--- minimum-absolute-difference.go
+++ minimum-absolute-difference.go
@@ -1,3 +1,32 @@
 package main
 
+func minimumAbsoluteDifference(arr []int32) int32 {
+	arrLength := int32(len(arr))
+	var minDiff int32
+	var tempDiff int32
+	var j int32
+	minDiff = absVal(arr[0] - arr[1])
+
+	for i := int32(0); i < arrLength; i++ {
+		j = i + 1
+		for j < arrLength {
+			tempDiff = absVal(arr[i] - arr[j])
+
+			if tempDiff < minDiff {
+				minDiff = tempDiff
+			}
+			j++
+		}
+	}
+
+	return minDiff
+}
+
+func absVal(a int32) int32 {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
 func main() {}
--- spec-minimum-absolute-difference_test.go
+++ spec-minimum-absolute-difference_test.go
@@ -2,6 +2,42 @@ package main
 
 import "testing"
 
-func TestMinimumAbsoluteDifference(t *testing.T) {
+func TestMinimumAbsoluteDifference1(t *testing.T) {
+	arr := []int32{3, -7, 0}
 
+	minDiff := minimumAbsoluteDifference(arr)
+
+	if minDiff != 3 {
+		t.Errorf("got %d instead of 3", minDiff)
+	}
+}
+
+func TestMinimumAbsoluteDifference2(t *testing.T) {
+	arr := []int32{-59, -36, -13, 1, -53, -92, -2, -96, -54, 75}
+
+	minDiff := minimumAbsoluteDifference(arr)
+
+	if minDiff != 1 {
+		t.Errorf("got %d instead of 1", minDiff)
+	}
+}
+
+func TestMinimumAbsoluteDifference3(t *testing.T) {
+	arr := []int32{1, -3, 71, 68, 17}
+
+	minDiff := minimumAbsoluteDifference(arr)
+
+	if minDiff != 3 {
+		t.Errorf("got %d instead of 3", minDiff)
+	}
+}
+
+func TestMinimumAbsoluteDifference4(t *testing.T) {
+	arr := []int32{-2, 2, 4}
+
+	minDiff := minimumAbsoluteDifference(arr)
+
+	if minDiff != 2 {
+		t.Errorf("got %d instead of 2", minDiff)
+	}
 }
