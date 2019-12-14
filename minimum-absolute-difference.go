6045b25f81103e2bf00570c007dde9ddf1ff5e88
John McKee <jmckee3@mac.com>
2019-12-13 16:13:03 -0800

    sort first

--- minimum-absolute-difference.go
+++ minimum-absolute-difference.go
@@ -4,18 +4,16 @@ func minimumAbsoluteDifference(arr []int32) int32 {
 	arrLength := int32(len(arr))
 	var minDiff int32
 	var tempDiff int32
-	var j int32
+
 	minDiff = absVal(arr[0] - arr[1])
 
-	for i := int32(0); i < arrLength; i++ {
-		j = i + 1
-		for j < arrLength {
-			tempDiff = absVal(arr[i] - arr[j])
+	arr = mergeSort(arr)
 
-			if tempDiff < minDiff {
-				minDiff = tempDiff
-			}
-			j++
+	for i := int32(0); i < arrLength-1; i++ {
+		tempDiff = absVal(arr[i] - arr[i+1])
+
+		if tempDiff < minDiff {
+			minDiff = tempDiff
 		}
 	}
 
@@ -29,4 +27,56 @@ func absVal(a int32) int32 {
 
 	return a
 }
+
+func mergeSort(arr []int32) []int32 {
+	arrLength := int32(len(arr))
+
+	if arrLength == 1 {
+		return arr
+	}
+
+	arrMiddle := arrLength / 2
+	var left = make([]int32, arrMiddle)
+	var right = make([]int32, arrLength-arrMiddle)
+
+	for i := int32(0); i < arrLength; i++ {
+		if i < arrMiddle {
+			left[i] = arr[i]
+		} else {
+			right[i-arrMiddle] = arr[i]
+		}
+	}
+
+	return merge(mergeSort(left), mergeSort(right))
+}
+
+func merge(left, right []int32) []int32 {
+	result := make([]int32, len(left)+len(right))
+
+	i := int32(0)
+
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] <= right[0] {
+			result[i] = left[0]
+			left = left[1:]
+		} else {
+			result[i] = right[0]
+			right = right[1:]
+		}
+		i++
+	}
+
+	for j := 0; j < len(left); j++ {
+		result[i] = left[j]
+		i++
+	}
+
+	for j := 0; j < len(right); j++ {
+		result[i] = right[j]
+		i++
+	}
+
+	return result
+}
+
 func main() {}
